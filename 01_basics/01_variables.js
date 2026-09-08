4109dba60b5070944b73964f5a53bbf07959bcda
Saniya <saniyapilankar@gmail.com>
2026-09-08 12:16:31 +0530

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId =1234567
+var accountPassword = "197543"
+let accountEmail = "saniyapilankar@gmail.com"
+
+//accountId = 4 // not allowed
+console.log(accountId);
+/* prefer not to use var, use let or const instead */
+  console.log(typeof accountId);
